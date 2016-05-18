0ebe397e75f52671ff72b833520f56e3a3e70916
Jeremy Byron <jeremy.byron@sixthimpulse.com>
2016-05-18 01:22:44 -0300

    WCF service created.  Launches in IIS Express and WCF Client test application consumes it.

---
+++ SimpleWcfService/ISimpleWcfService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleWcfService {
+    
+[ServiceContract]
+public interface ISimpleWcfService {
+
+    /// <summary>
+    /// Return the current time on the server, expressed in the specified timezone
+    /// </summary>
+    /// <param name="timezoneName">The timezone ID you want to retrieve.  Leave blank to use server's timezone.  List of IDs can be found here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones </param>
+    /// <returns></returns>
+    [OperationContract]
+    DateTime GetServerDate(string timezoneName);
+
+    /// <summary>
+    /// Similar to GetServerDate, except this function returns details on the requested timezone
+    /// </summary>
+    /// <param name="timezoneName"></param>
+    /// <returns></returns>
+    [OperationContract]
+    TimeWithTimezone GetServerDateWithTzInfo(string timezoneName);
+
+    // TODO: Add your service operations here
+}
+
+
+[DataContract]
+public class TimeWithTimezone {
+    DateTime m_time;
+    TimeZoneInfo m_tzi;
+
+    [DataMember]
+    public DateTime TheTime {
+        get { return m_time; }
+        set { m_time = value; }
+    }
+
+    [DataMember]
+    public TimeZoneInfo TimezoneDetails {
+        get { return m_tzi; }
+        set { m_tzi = value; }
+    }
+}
+}
---
+++ SimpleWcfService/SimpleWcfService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleWcfService {
+
+public class SimpleWcfService : ISimpleWcfService {
+    
+    public DateTime GetServerDate(string timezoneName) {
+        return GetServerDateWithTzInfo(timezoneName).TheTime;
+    }
+
+    public TimeWithTimezone GetServerDateWithTzInfo(string timezoneName) {
+        TimeWithTimezone rv = new TimeWithTimezone();
+        TimeZone tz = null;
+
+        if(string.IsNullOrWhiteSpace(timezoneName)) {
+            tz = TimeZone.CurrentTimeZone;
+            rv.TheTime = tz.ToLocalTime(DateTime.Now);
+            rv.TimezoneDetails = TimeZoneInfo.FindSystemTimeZoneById(tz.StandardName);
+        } else {
+            try {
+                TimeZoneInfo tzi = TimeZoneInfo.FindSystemTimeZoneById(timezoneName);
+                
+                rv.TheTime = DateTime.UtcNow.AddMinutes(tzi.GetUtcOffset(DateTime.UtcNow).TotalMinutes);
+                rv.TimezoneDetails = tzi;
+            } catch (TimeZoneNotFoundException) {
+                tz = TimeZone.CurrentTimeZone;
+                rv.TheTime = tz.ToLocalTime(DateTime.Now);
+                rv.TimezoneDetails = TimeZoneInfo.FindSystemTimeZoneById(tz.StandardName);
+            }
+        } // if
+
+        return rv;
+    }
+}
+}
