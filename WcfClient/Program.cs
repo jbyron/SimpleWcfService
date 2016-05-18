0307337dfe2795bb31140ef297434ac55ab78c4b
Jeremy Byron <jeremy.byron@sixthimpulse.com>
2016-05-18 19:39:20 -0300

    WCF Client added

---
+++ WcfClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfClient.SixthImpulse.SimpleWcf.WcfService;
+
+namespace WcfClient {
+    class Program {
+        static void Main(string[] args) {
+
+            ISimpleWcfService svc = new SimpleWcfServiceClient();
+            TimeWithTimezone ttz = svc.GetServerDateWithTzInfo("");
+            Console.WriteLine(string.Format("Server Local Time Is: {0} {1} GMT+{2}", ttz.TheTime, ttz.TimezoneName, ttz.TimezoneGmtOffset));
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            return;
+        }
+    }
+}
